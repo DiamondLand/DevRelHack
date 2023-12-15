953caa40b060e1d30d551b4de13086ffa298eb50
Alexey Ovchinnikov <119781637+allelleo@users.noreply.github.com>
2023-12-16 01:31:13 +0300

    Delete src directory

--- src/client/controller.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-
-client = APIRouter()
-
-templates = Jinja2Templates(directory="public/templates")
--- src/user/oauth/controller.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter, Request
-from user.oauth.api.v1.controller import oauth
-
-user_auth = APIRouter()
-user_auth.include_router(oauth, prefix='/oauth', tags=['auth'])
