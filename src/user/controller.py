d8a514ae17b9faa823787c7b4cfc7f2dda6a5a41
Alexey Ovchinnikov <119781637+allelleo@users.noreply.github.com>
2023-12-16 01:33:46 +0300

    Delete src directory

--- src/user/controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from user.oauth.controller import user_auth
-from user.api.v1.controller import v1
-from user.api.v1.admin import v1 as v1_admin
-
-user = APIRouter()
-user.include_router(user_auth)
-user.include_router(v1)
-user.include_router(v1_admin)
